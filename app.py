d61b9630ce9fd817900162b60f60878fa5527da2
DAKSH PANDEY <dakshpandey5257@gmail.com>
2026-09-04 14:56:09 +0530

    Fix production CORS origin

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://fraud-detection-xgb.netlify.app/"], # Allows your React app
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://fraud-detection-xgb.netlify.app"], # Allows your React app
     allow_credentials=True,
     allow_methods=["*"], # Allows POST, GET, OPTIONS, etc.
     allow_headers=["*"],
